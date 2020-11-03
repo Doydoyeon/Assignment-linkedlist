682a7798a1a005311bbd33875c00a19d344986cc
Doyeon Kim <doyeon0918@gmail.com>
2020-11-03 20:25:19 +0100

    Case 1 done
    insert

--- Linkedlist_DoyeonKim.py
+++ Linkedlist_DoyeonKim.py
@@ -4,7 +4,24 @@ class Node:
     def __init__(self, val):
         self.val = val
         self.next = None
-        
 
+    # case 1
+    # a->None,
+    # a.insertNext (b)
+    # a-> b
+
+    # case 2
+    # a -> c
+    # a -> b -> c
+
+    def insert(self, right):
+        if self.next == None:
+            self.next = right
+
+        # else:
+        #     if
 
 a = Node(10)
+b = Node(7)
+a.insert(b)
+print("finish")
