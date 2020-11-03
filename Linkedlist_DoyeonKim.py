3d7e323853997e01aeaa4f70d7434059ffa462af
Doyeon Kim <doyeon0918@gmail.com>
2020-11-03 20:49:50 +0100

    Case 2
    insert when next is not None

--- Linkedlist_DoyeonKim.py
+++ Linkedlist_DoyeonKim.py
@@ -11,17 +11,23 @@ class Node:
     # a-> b
 
     # case 2
-    # a -> c
+    # initial state: a -> c
+    # a.insert (b) : a -> b ->
     # a -> b -> c
 
     def insert(self, right):
         if self.next == None:
             self.next = right
 
-        # else:
-        #     if
+        else:
+            temp = self.next
+            self.next = right
+            right.next = temp
+
 
 a = Node(10)
 b = Node(7)
+c = Node (3)
+a.insert(c)
 a.insert(b)
 print("finish")
